a5d878f23396da3bfd6ee67d92375bc73881de38
Amaldev-ca <120363658+Amaldev-ca@users.noreply.github.com>
2023-02-17 11:19:40 +0530

    Delete co5eg.py

--- cycle5/co5eg.py
+++
@@ -1,7 +0,0 @@
-import csv
-with open('studentdetails.csv','r')as csv_file1:
-    csv_reader=csv.reader(csv_file1)
-    with open('newstudentdetails1.csv','w')as csv_file2:
-        csv_writer=csv.writer(csv_file2)
-        for line in csv_reader:
-            csv_writer.writerow(line)
